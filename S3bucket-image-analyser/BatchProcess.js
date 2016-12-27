8ae6fde9bf41b903c4229574cc57581cbf50b820
Sambasiva.Reddy <sambasiva.reddy@uk.tesco.com>
2016-12-27 22:17:31 +0000

    Added a proceject to index the existing S3 image files

---
+++ S3bucket-image-analyser/BatchProcess.js
@@ -0,0 +1,37 @@
+const AWS = require('aws-sdk');
+const config = require('./app-config.js');
+const request = require('request');
+
+const configPath = config.configPath;
+const bucketName = config.bucket;
+const prefix = config.prefix;
+
+AWS.config.loadFromPath(configPath);
+let s3 = new AWS.S3();
+var bucketParams = {Bucket: bucketName, Prefix: prefix};
+
+s3.listObjects(bucketParams, (err, data) => {
+    let bucketContents = data.Contents || [];
+    bucketContents.forEach(content => {
+        const URL = `${s3.endpoint.href}${bucketName}/${content.Key}`;
+        console.log(URL);
+        if (!URL.endsWith("/")) {
+
+            let options = {
+                url: config.URL,
+                headers: {
+                    'url': URL
+                },
+                method: "POST"
+            };
+            request(options, (error, response, body) => {
+                let message = body;
+                if (error) {
+                    message = error;
+                }
+                console.log(message);
+            });
+        }
+    });
+
+})
---
+++ S3bucket-image-analyser/app-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    URL: "http://localhost:9000/analyse",
+    bucket: "digital-photoframe-lib",
+    prefix: "uploads",
+    configPath: "config.json"
+}
