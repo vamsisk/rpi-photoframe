0830b495849cf976b2d98c52e704def9ec0b35d7
Sambasiva.Reddy <sambasiva.reddy@uk.tesco.com>
2016-12-28 11:23:23 +0000

    Added gps and caption to index in solr

--- photo-analyser/lib/SolrClient.js
+++ photo-analyser/lib/SolrClient.js
@@ -6,28 +6,49 @@ module.exports = SolrClient => (imageData) => {
     let exif = imageData.imageInfo.exif || {};
 
     let data = {
-        path: imageData.path,
-        Make: imageData.imageInfo.image.Make,
-        Model: imageData.imageInfo.image.Model,
-        XResolution: imageData.imageInfo.image.imageInfoXResolution,
-        YResolution: imageData.imageInfo.image.YResolution,
-        DateTimeOriginal: exif.DateTimeOriginal,
-        CreateDate: exif.CreateDate,
-        width: imageData.visionInfo.metadata.width,
-        height: imageData.visionInfo.metadata.height,
-        format: imageData.visionInfo.metadata.format,
-
-        categories: [],
-        tags: []
+            path: imageData.path,
+            Make: imageData.imageInfo.image.Make,
+            Model: imageData.imageInfo.image.Model,
+            XResolution: imageData.imageInfo.image.imageInfoXResolution,
+            YResolution: imageData.imageInfo.image.YResolution,
+            DateTimeOriginal: exif.DateTimeOriginal,
+            CreateDate: exif.CreateDate,
+            width: imageData.visionInfo.metadata.width || imageData.imageInfo.exif.ExifImageWidth || 0,
+            height: imageData.visionInfo.metadata.height || imageData.imageInfo.exif.ExifImageHeight || 0,
+            format: imageData.visionInfo.metadata.format,
     };
 
-    imageData.visionInfo.categories.forEach(category => {
-        data.categories.push(category.name);
-    })
+   try {
+        if (imageData.visionInfo.categories.length > 0) {
+            data.categories = [];
+            imageData.visionInfo.categories.forEach(category => {
+                data.categories.push(category.name);
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
 
-    imageData.visionInfo.tags.forEach(tag => {
-        data.tags.push(tag.name);
-    })
+
+    try {
+        if (imageData.visionInfo.tags.length >= 0) {
+            data.tags = [];
+            imageData.visionInfo.tags.forEach(tag => {
+                data.tags.push(tag.name);
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+
+    try {
+        if (Object.keys(imageData.imageInfo.exif.gps).length > 0) {
+            data.gps = imageData.imageInfo.exif.gps;
+        }
+    } catch (error) {
+        console.log(error);
+    }
 
     indexData.push(data);
 
