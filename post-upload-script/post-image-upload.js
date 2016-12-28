c911caabd0693ad03e852552c5f3283dea846c40
Sambasiva.Reddy <sambasiva.reddy@uk.tesco.com>
2016-12-28 20:49:03 +0000

    made separate folder for post script

--- post-upload-script/post-image-upload.js
+++ post-upload-script/post-image-upload.js
@@ -1,14 +1,14 @@
 var request = require('request');
 exports.handler = (event, context, callback) => {
-    event.Records.forEach(record =>{
+    event.Records.forEach(record => {
         const imageURL = `https://s3.${record.awsRegion}.amazonaws.com/${record.s3.bucket.name}/${record.s3.object.key}`;
         const options = {
-                url: "http://52.15.136.66:9000/analyse",
-                headers: {
-                    'url': imageURL
-                },
-                method: "POST"
-            };
+            url: "http://52.15.136.66:9000/analyse",
+            headers: {
+                'url': imageURL
+            },
+            method: "POST"
+        };
         request(options, (error, response, body) => {
             if (error) {
                 console.log(error);
@@ -16,8 +16,7 @@ exports.handler = (event, context, callback) => {
 
         });
     });
-    
-   
-     
+
+
     callback(null, 'Successfully executed lambda');
 };
